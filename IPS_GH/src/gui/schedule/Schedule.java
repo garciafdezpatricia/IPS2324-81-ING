65a317d6900f253782d9fa657dd0f4879eeb2ffa
Andrés Cadenas <andrescabla@gmail.com>
2023-10-13 14:34:00 +0200

    Almost finished Schedule

--- IPS_GH/src/gui/schedule/Schedule.java
+++ IPS_GH/src/gui/schedule/Schedule.java
@@ -12,6 +12,11 @@ import javax.swing.JScrollPane;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
 import com.toedter.calendar.JCalendar;
+
+import db.Appointment;
+import oracle.sql.DATE;
+import util.AppointmentBLDto;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.border.EmptyBorder;
@@ -20,7 +25,11 @@ import javax.swing.AbstractListModel;
 import javax.swing.border.LineBorder;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import javax.swing.ScrollPaneConstants;
 
 public class Schedule extends JFrame {
 
@@ -28,13 +37,17 @@ public class Schedule extends JFrame {
 	private JPanel leftPanel;
 	private JPanel rightPanel;
 	private JLabel lblDescription;
-	private JScrollPane scrollPane;
+	private JScrollPane pnAppointments;
 	private JCalendar calendar;
 	private JLabel lblDate;
-	private JButton btnGoBack;
-	private JList list;
 	private JButton btnViewInfo;
-	private List<String> appointments;
+	private List<AppointmentBLDto> appointments = null;
+	private List<AppointmentBLDto> filteredApmnts = null;
+	private static int doctorId = 1;
+	
+	private JPanel panel;
+	private JButton btnBack;
+	private JButton btnFilter;
 
 	/**
 	 * Launch the application.
@@ -42,9 +55,11 @@ public class Schedule extends JFrame {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				try {
+				try {										
 					Schedule frame = new Schedule();
 					frame.setVisible(true);
+					
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -52,10 +67,39 @@ public class Schedule extends JFrame {
 		});
 	}
 
+	private void filterAppointments() {
+		var filtered = new ArrayList<AppointmentBLDto>();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String format = formatter.format(getCalendar().getDate());
+		for(AppointmentBLDto apmnt: appointments) {		
+	        var apDate = apmnt.startDate.split(" ");
+			if(apDate[0].equals(format)) {
+				filtered.add(apmnt);
+			
+			}
+		}
+		this.filteredApmnts = filtered;
+		
+		getPnAppointments().removeAll();
+		for(AppointmentBLDto p : filteredApmnts) {
+			System.out.println(p);
+			getPnAppointments().add(newButton(p));
+		}
+	}
+	
+	
+	private JButton newButton(AppointmentBLDto p) {
+		JButton button = new JButton(p.toString());
+		button.setVisible(true);
+		return button;
+	}
 	/**
 	 * Create the frame.
 	 */
 	public Schedule() {
+
+		appointments = Appointment.getAppointmentsByDoctorId(doctorId);	
+		filterAppointments();
 		setBounds(100, 100, 498, 340);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
@@ -70,7 +114,7 @@ public class Schedule extends JFrame {
 			leftPanel.setLayout(new BorderLayout(0, 0));
 			leftPanel.add(getCalendar());
 			leftPanel.add(getLblDate(), BorderLayout.NORTH);
-			leftPanel.add(getBtnGoBack(), BorderLayout.SOUTH);
+			leftPanel.add(getPanel(), BorderLayout.SOUTH);
 		}
 		return leftPanel;
 	}
@@ -80,8 +124,8 @@ public class Schedule extends JFrame {
 			rightPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 			rightPanel.setLayout(new BorderLayout(0, 0));
 			rightPanel.add(getLblDescription(), BorderLayout.NORTH);
-			rightPanel.add(getScrollPane(), BorderLayout.CENTER);
-			rightPanel.add(getBtnViewInfo(), BorderLayout.SOUTH);
+			rightPanel.add(getPnAppointments(), BorderLayout.CENTER);
+		
 		}
 		return rightPanel;
 	}
@@ -91,12 +135,17 @@ public class Schedule extends JFrame {
 		}
 		return lblDescription;
 	}
-	private JScrollPane getScrollPane() {
-		if (scrollPane == null) {
-			scrollPane = new JScrollPane();
-			scrollPane.setViewportView(getList());
+	private JScrollPane getPnAppointments() {
+		if (pnAppointments == null) {
+			pnAppointments = new JScrollPane();
+			pnAppointments.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+			pnAppointments.removeAll();
+			for(AppointmentBLDto p : filteredApmnts) {
+				System.out.println(p);
+				pnAppointments.add(newButton(p));
+			}
 		}
-		return scrollPane;
+		return pnAppointments;
 	}
 	private JCalendar getCalendar() {
 		if (calendar == null) {
@@ -112,48 +161,36 @@ public class Schedule extends JFrame {
 		}
 		return lblDate;
 	}
-	private JButton getBtnGoBack() {
-		if (btnGoBack == null) {
-			btnGoBack = new JButton("Back to Menu");
+	
+	
+
+	private JPanel getPanel() {
+		if (panel == null) {
+			panel = new JPanel();
+			panel.add(getBtnBack());
+			panel.add(getBtnFilter());
 		}
-		return btnGoBack;
+		return panel;
 	}
-	private JList getList() {
-		if (list == null) {
-			list = new JList();
-			list.setModel(new AbstractListModel() {
-				String[] values = new String[] {"a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "s", "a", "a"};
-				public int getSize() {
-					return values.length;
-				}
-				public Object getElementAt(int index) {
-					return values[index];
-				}
-			});
-			list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+	private JButton getBtnBack() {
+		if (btnBack == null) {
+			btnBack = new JButton("Back");
 		}
-		return list;
+		return btnBack;
 	}
-	private JButton getBtnViewInfo() {
-		if (btnViewInfo == null) {
-			btnViewInfo = new JButton("Open Appointment");
-			btnViewInfo.addMouseListener(new MouseAdapter() {
+	private JButton getBtnFilter() {
+		if (btnFilter == null) {
+			btnFilter = new JButton("Filter Appointments");
+			btnFilter.addMouseListener(new MouseAdapter() {
 				@Override
 				public void mouseClicked(MouseEvent e) {
-					loadSelectedAppointment();
+					
+					filterAppointments();
+			        
+			       
 				}
-
-				
 			});
 		}
-		return btnViewInfo;
+		return btnFilter;
 	}
-	
-	private void loadSelectedAppointment() {
-		if(getList().getSelectedIndex() != -1) {
-			//loadAppointment(appointments.get(getList().getSelectedIndex()));
-		}
-	}
-
-	
 }
